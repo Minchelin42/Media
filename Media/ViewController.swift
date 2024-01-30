1ef92604a37de6aeff00b144621585e2e48a3d2a
Minchelin42 <mje0211@naver.com>
2024-01-30 20:16:54 +0900

    Initial Commit

---
+++ Media/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Media
+//
+//  Created by 민지은 on 2024/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
