014c97d69a596a164f8505e3d7868a716b602f13
Minchelin42 <mje0211@naver.com>
2024-02-03 23:48:11 +0900

    DESIGN: SearchViewController 추가

--- Media/Search/SearchView.swift
+++ Media/Search/SearchView.swift
@@ -7,14 +7,51 @@
 
 import UIKit
 
-class SearchView: UIView {
+class SearchView: BaseView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let searchBar = UISearchBar()
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: configureCollectionViewLayout())
+    
+    var resultList: [TV] = []
+    
+    override func configureHierarchy() {
+        addSubview(searchBar)
+        addSubview(collectionView)
     }
-    */
+    
+    override func configureLayout() {
+        searchBar.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(safeAreaLayoutGuide)
+        }
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.horizontalEdges.bottom.equalTo(safeAreaLayoutGuide)
+        }
+    }
+    
+    override func configureView() {
+        searchBar.barStyle = .black
+        collectionView.backgroundColor = .clear
+        
+        collectionView.register(PosterCollectionViewCell.self, forCellWithReuseIdentifier: "Poster")
+    }
+    
+    
+    func configureCollectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: (UIScreen.main.bounds.width - 20) / 3, height: 180)
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 0
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        layout.scrollDirection = .vertical
 
+        return layout
+        
+    }
+
+}
+
+#Preview {
+    SearchViewController()
 }
--- Media/Search/SearchViewController.swift
+++ Media/Search/SearchViewController.swift
@@ -6,24 +6,67 @@
 //
 
 import UIKit
+import Kingfisher
 
 class SearchViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    let mainView = SearchView()
+    
+    override func loadView() {
+        self.view = mainView
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+        
+        mainView.searchBar.delegate = self
+        
+        mainView.collectionView.delegate = self
+        mainView.collectionView.dataSource = self
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
+}
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return mainView.resultList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Poster", for: indexPath) as! PosterCollectionViewCell
+        
+        let imageBaseURL = "https://image.tmdb.org/t/p/w500"
+        let item = mainView.resultList[indexPath.row]
+        
+        if let url = item.poster_path {
+            cell.posterImageView.kf.setImage(with: URL(string: imageBaseURL + "\(url)"), placeholder: UIImage(systemName: "suit.heart"))
+        } else {
+            cell.posterImageView.image = UIImage(systemName: "xmark")
+        }
+        
+        cell.posterNameLabel.isHidden = true
+        cell.textLabel.isHidden = true
+
+        return cell
+    }
+
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        TVAPIManager.shared.getTVAPI(api: .search(query: searchBar.text!)) { result in
+            self.mainView.resultList = result.results
+            self.mainView.collectionView.reloadData()
+            self.view.endEditing(true)
+        }
+    }
+}
+
+#Preview {
+    
+    SearchViewController()
+    
 }
